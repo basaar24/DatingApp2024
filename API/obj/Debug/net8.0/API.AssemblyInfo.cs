6b8b810e0692917da97a785bc1e810692ce41ac4
eriki26 <erick.gabriel0204@gmail.com>
2024-09-03 00:02:00 -0600

    Parcial01 - Add database using dotnet ef database command

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7bbd905bcf96228837f676257515cf1e5aeac97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f52ee86950c16604e67a90f143fdb7b9f776e9d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
