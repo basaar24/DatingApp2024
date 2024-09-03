b7bbd905bcf96228837f676257515cf1e5aeac97
eriki26 <erick.gabriel0204@gmail.com>
2024-09-02 22:47:23 -0600

    Parcial01 -Add DataContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext
+    public class DataContext(DbContextOptions options) : DbContext(options)
     {
-        
+        public DbSet<AppUser> Users { get; set; }
     }
 }
