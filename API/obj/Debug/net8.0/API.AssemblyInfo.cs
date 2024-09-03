f52ee86950c16604e67a90f143fdb7b9f776e9d0
eriki26 <erick.gabriel0204@gmail.com>
2024-09-02 23:39:37 -0600

    Parcial01 - Add first EF migration

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205ca90bc5d51eccbb0ee56fc8224d7f90570dab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7bbd905bcf96228837f676257515cf1e5aeac97")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
