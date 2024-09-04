4e2f5157c8ce94b773e938728f7d1b8f58e58335
eriki26 <erick.gabriel0204@gmail.com>
2024-09-03 23:37:52 -0600

    Parcial01 - Add Async code

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8b810e0692917da97a785bc1e810692ce41ac4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273f319c056576047dedb8e064f6ff99ecee940d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
