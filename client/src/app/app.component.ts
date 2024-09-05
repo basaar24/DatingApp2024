9c416eed80e996d66a625c23fc4787b3c2646af4
Arturo Barajas <basaar.24@gmail.com>
2024-09-05 07:10:37 -0600

    Parcial01 - Add first Angular component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'Date me';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/v1/users").subscribe({
+      next: (response) => { this.users = response },
+      error: (error) => { console.log(error) },
+      complete: () => { console.log("Request completed!") }
+    });
+  }
 }
