1d0659b261103671e611b14ef27feb9547eec7c9
eriki26 <erick.gabriel0204@gmail.com>
2024-09-22 23:24:48 -0600

    Parcial01 - Add password to appuser

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public required string UserName { get; set; }
+        public required byte[] PasswordHash { get; set; }
+        public required byte[] PasswordSalt { get; set; }
     }
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf0ab02f13bca15e32f12dc00e74b8c0b116957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a27242f9f806c9009bd2ac720c61952e8c8f99e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
