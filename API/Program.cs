62f7a896127da51daac19e3cb979e2378b3031dc
eriki26 <erick.gabriel0204@gmail.com>
2024-09-22 19:34:28 -0600

    Parcial 01 - Add CORS

--- API/Program.cs
+++ API/Program.cs
@@ -9,8 +9,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors((cors) => cors
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins(
+        "http://localhost:4200",
+        "https://localhost:4200"));
 
 app.MapControllers();
 
