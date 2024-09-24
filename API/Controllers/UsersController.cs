ec4b21814864a7089165b51917fffbc698fb404c
Arturo Barajas <basaar.24@gmail.com>
2024-09-24 07:51:10 -0600

    Parcial01 - Add nav bar to Angular SPA

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -26,7 +26,7 @@ public class UsersController : BaseApiController
     }
 
     [Authorize]
-    [HttpGet("{id:int}")] // api/v1/users/2
+    [HttpGet("{id:int}")] // api/users/2
     public async Task<ActionResult<AppUser>> GetUsersByIdAsync(int id)
     {
         var user = await _context.Users.FindAsync(id);
@@ -36,7 +36,7 @@ public class UsersController : BaseApiController
         return user;
     }
     
-    [HttpGet("{name}")] // api/v1/users/Calamardo
+    [HttpGet("{name}")] // api/users/Calamardo
     public ActionResult<string> Ready(string name)
     {
         return $"Hi {name}";
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
   users: any;
 
   ngOnInit(): void {
-    this.http.get("https://localhost:5001/api/v1/users").subscribe({
+    this.http.get("https://localhost:5001/api/users").subscribe({
       next: (response) => { this.users = response },
       error: (error) => { console.log(error) },
       complete: () => { console.log("Request completed!") }
