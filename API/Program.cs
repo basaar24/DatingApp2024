20b77579fb710ddb1c51389fd4e960bc565055e1
eriki26 <erick.gabriel0204@gmail.com>
2024-09-23 20:32:33 -0600

    Parcial 01 - Add Token ervice

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
