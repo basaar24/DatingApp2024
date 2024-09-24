12bcdd7599fa99072c9e46bd3cc146e7ebb3361a
Arturo Barajas <basaar.24@gmail.com>
2024-09-24 07:18:02 -0600

    Parcial01 - Add extension methods

--- API/Program.cs
+++ API/Program.cs
@@ -8,26 +8,8 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options => 
-    {
-        var tokenKey = builder.Configuration["TokenKey"]
-            ?? throw new ArgumentNullException(nameof(builder.Configuration["TokenKey"]));
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
