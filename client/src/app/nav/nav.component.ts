717125049d0a3927c05ee205e2d13e6f59eeaa96
eriki26 <erick.gabriel0204@gmail.com>
2024-10-01 23:55:04 -0600

    Parcial 01 - Angular template forms

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  model: any = {};
 
+  login(): void{
+    console.log(this.model);
+  }
 }
