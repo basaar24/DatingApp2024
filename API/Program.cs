6c1bf0cf89e84b80626525baebec232f6c7a6949
eriki26 <erick.gabriel0204@gmail.com>
2024-10-01 23:28:08 -0600

    Parcial 01 - Add nav bar to Angular app

--- API/Program.cs
+++ API/Program.cs
@@ -21,19 +21,6 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        var tokenKey = builder.Configuration["TokenKey"] 
-            ?? throw new ArgumentNullException("TokenKey");
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("tokenKey")),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
 
 var app = builder.Build();
 
