677afda33c87fb27a5a564533c7b1a20b274179a
eriki26 <erick.gabriel0204@gmail.com>
2024-10-06 22:30:45 -0600

    Parcial 01- PARENT to CHILD communication

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  usersFromHomeComponent = input.required<any>();
   model: any = {};
 
   register(): void {
