3689a701d07598a5aa6a4c462b7f235d8d57cd98
Arturo Barajas <basaar.24@gmail.com>
2024-10-07 07:36:23 -0600

    Parcial02 - Add routing and components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "members", component: MemberListComponent},
+    {path: "members/:id", component: MemberDetailComponent},
+    {path: "lists", component: ListsComponent},
+    {path: "messages", component: MessagesComponent},
+    {path: "**", component: HomeComponent, pathMatch: "full"},
+];
