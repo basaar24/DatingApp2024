1f463540f0286987d0eb2f21fcc200163a981299
eriki26 <erick.gabriel0204@gmail.com>
2024-10-06 19:58:50 -0600

    Parcial 01 - Persist the login

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable, signal } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,21 @@ import { Observable } from 'rxjs';
 export class AccountService {
   private http = inject(HttpClient);
   baseUrl = "https://localhost:5001/api/";
+  currentUser = signal<User | null>(null);
 
-  login(model: any): Observable<Object> {
-    return this.http.post(this.baseUrl + "account/login", model);
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + "account/login", model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+    this.currentUser.set(null);
   }
 }
