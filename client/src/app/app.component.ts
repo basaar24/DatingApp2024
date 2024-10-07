9e867db0fb326405613df1383057a4c518ccc1b6
eriki26 <erick.gabriel0204@gmail.com>
2024-10-06 20:37:23 -0600

    Parcial 01 - Add home page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { Observable } from 'rxjs';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -23,7 +24,7 @@ export class AppComponent implements OnInit{
     this.setCurrentUser();
   }
 
-  setCurrentUser() {
+  setCurrentUser(): void {
     const userString = localStorage.getItem("user");
     if (!userString) return;
     const user = JSON.parse(userString);
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  registerMode = false;
+
+  registerToggle(): void {
+    this.registerMode = !this.registerMode;
+  }
+
+}
