57b479ee75f4c355ae2e363468f4d571a05f0d46
Arturo Barajas <basaar.24@gmail.com>
2024-10-15 07:12:33 -0600

    Parcial02 - Add validation error section

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -11,6 +11,7 @@ import { Component, inject } from '@angular/core';
 export class TestErrorsComponent {
   baseUrl = "https://localhost:5001/api/";
   private http = inject(HttpClient);
+  validationErrors: string[] = [];
 
   get400Error(): void {
     this.http.get(this.baseUrl + "buggy/bad-request").subscribe({
@@ -43,7 +44,10 @@ export class TestErrorsComponent {
   get400ValidationError(): void {
     this.http.post(this.baseUrl + "account/register", {}).subscribe({
       next: (response) => console.log(response),
-      error: (error) => console.log(error)
+      error: (error) => {
+        console.log(error);
+        this.validationErrors = error;
+      }
     })
   }
 }
