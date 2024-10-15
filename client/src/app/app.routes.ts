5e557698ed6f0bd74f285a2b4f278a8685b5f43e
Arturo Barajas <basaar.24@gmail.com>
2024-10-15 07:21:18 -0600

    Parcial02 - Add not-found and server-error landing pages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
@@ -21,5 +23,7 @@ export const routes: Routes = [
         ]
     },
     {path: "errors", component: TestErrorsComponent},
+    {path: "not-found", component: NotFoundComponent},
+    {path: "server-error", component: ServerErrorComponent},
     {path: "**", component: HomeComponent, pathMatch: "full"},
 ];
---
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-error.component.html',
+  styleUrl: './server-error.component.css'
+})
+export class ServerErrorComponent {
+
+}
