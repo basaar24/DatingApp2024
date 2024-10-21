e5cac130893dd016623ff747cefbfbb1c2bedbbb
Arturo Barajas <basaar.24@gmail.com>
2024-10-21 07:55:02 -0600

    Parcial00 - Add AutoMapper to UsersController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ namespace API.Controllers;
 using API.Data;
 using API.DataEntities;
 using API.DTOs;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,12 @@ using Microsoft.AspNetCore.Mvc;
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _repository;
+    private readonly IMapper _mapper;
 
-    public UsersController(IUserRepository repository)
+    public UsersController(IUserRepository repository, IMapper mapper)
     {
         _repository = repository;
+        _mapper = mapper;
     }
 
     [HttpGet]
@@ -20,7 +23,9 @@ public class UsersController : BaseApiController
     {
         var users = await _repository.GetAllAsync();
 
-        return Ok(users);
+        var response = _mapper.Map<IEnumerable<MemberResponse>>(users);
+
+        return Ok(response);
     }
 
     [HttpGet("{id:int}")] // api/users/2
@@ -33,7 +38,7 @@ public class UsersController : BaseApiController
             return NotFound();
         }
 
-        return user;
+        return _mapper.Map<MemberResponse>(user);
     }
 
     [HttpGet("{username}")] // api/users/Calamardo
@@ -46,6 +51,6 @@ public class UsersController : BaseApiController
             return NotFound();
         }
 
-        return user;
+        return _mapper.Map<MemberResponse>(user);
     }
 }
