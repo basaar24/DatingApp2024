ac47a40ed704ce41beb71f1acfbec0c1f7882182
Arturo Barajas <basaar.24@gmail.com>
2024-10-28 07:38:33 -0600

    Parcial03 - Add JWT Interceptor

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,30 +1,20 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
-import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   baseUrl = environment.apiUrl;
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + "users", this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + "users");
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + "users/" + username, this.getHttpOptions());
-  }
-
-  getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${this.accountService.currentUser()?.token}`
-      })
-    };
+    return this.http.get<Member>(this.baseUrl + "users/" + username);
   }
 }
