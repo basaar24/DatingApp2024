abd8524c6581bffd239b9e81c5581a157646386b
Arturo Barajas <basaar.24@gmail.com>
2024-10-30 15:50:22 -0600

    Parcial03 - Add photo gallery

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './_interceptors/error.interceptor';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
+import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +16,13 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideToastr({
       positionClass: "toast-bottom-right"
-    })
+    }),
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'contain'
+      } as GalleryConfig
+    }
   ]
 };
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,11 +3,12 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -15,6 +16,7 @@ export class MemberDetailComponent implements OnInit{
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
   member?: Member;
+  images: GalleryItem[] = [];
   
   ngOnInit(): void {
     this.loadMember();
@@ -24,7 +26,12 @@ export class MemberDetailComponent implements OnInit{
     const username = this.route.snapshot.paramMap.get("username");
     if (!username) return;
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: (member) => {
+        this.member = member;
+        member.photos.map((photo) => {
+          this.images.push(new ImageItem({src: photo.url, thumb: photo.url}));
+        });
+      }
     });
   }
 }
