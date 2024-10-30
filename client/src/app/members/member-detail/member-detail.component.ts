8c2652687b74a50cf6fce016d0445d501c8c4b9e
Arturo Barajas <basaar.24@gmail.com>
2024-10-30 07:18:53 -0600

    Parcial03 - Add style to member detail component

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [],
+  imports: [TabsModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
