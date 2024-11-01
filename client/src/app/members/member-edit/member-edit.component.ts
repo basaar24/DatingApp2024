9bf3de3545e0a39d65c86b4e773c66ff463f9c09
Arturo Barajas <basaar.24@gmail.com>
2024-11-01 07:26:07 -0600

    Parcial03 - Add update profile

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
@@ -13,9 +14,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './member-edit.component.css'
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild("editForm") editForm?: NgForm;
   member?: Member;
   private accountService = inject(AccountService);
   private membersService = inject(MembersService);
+  private toastr = inject(ToastrService);
 
   ngOnInit(): void {
     this.loadMember();
@@ -27,5 +30,11 @@ export class MemberEditComponent implements OnInit {
     this.membersService.getMember(user.username).subscribe({
       next: member => this.member = member
     })
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success("Profile updated!");
+    this.editForm?.reset(this.member);
   }
 }
