f1625d661c13f4951eede5abc54cf09da96c0a2c
Arturo Barajas <basaar.24@gmail.com>
2025-02-17 07:05:05 -0600

    Parcial04 - Add feedback to FormGroup

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,12 +2,12 @@ import { Component, inject, input, OnInit, output } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -24,7 +24,7 @@ export class RegisterComponent implements OnInit {
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl("Hola", Validators.required),
+      username: new FormControl("", Validators.required),
       password: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
       confirmPassword: new FormControl("", [Validators.required, this.matchValues("password")])
     });
