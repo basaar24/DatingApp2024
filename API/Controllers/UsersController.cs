a28e0391cd7feb8cfe89c3f05e0be616e610c546
Arturo Barajas <basaar.24@gmail.com>
2025-02-19 07:48:02 -0600

    Parcial04 - Update client side user registration

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -87,6 +87,11 @@ public class UsersController : BaseApiController
             PublicId = result.PublicId
         };
 
+        if (user.Photos.Count == 0)
+        {
+            photo.IsMain = true;
+        }
+
         user.Photos.Add(photo);
 
         if (await _repository.SaveAllAsync())
