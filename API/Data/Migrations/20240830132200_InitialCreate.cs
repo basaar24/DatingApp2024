51fa5fd04e8ecebf65228c1ae1bb6edb1567f27f
eriki26 <erick.gabriel0204@gmail.com>
2025-03-03 00:26:27 -0600

    Parcial04 - Initialize Reactive form

---
+++ API/Data/Migrations/20240830132200_InitialCreate.cs
@@ -0,0 +1,27 @@
+﻿#nullable disable
+
+namespace API.Data.Migrations
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    [ExcludeFromCodeCoverage]
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table => table.PrimaryKey("PK_Users", x => x.Id));
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropTable(
+                name: "Users");
+    }
+}
---
+++ API/Data/Migrations/20240917131340_UserPasswordAdded.cs
@@ -0,0 +1,42 @@
+﻿#nullable disable
+
+namespace API.Data.Migrations
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    [ExcludeFromCodeCoverage]
+    /// <inheritdoc />
+    public partial class UserPasswordAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: Array.Empty<byte>());
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: Array.Empty<byte>());
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+        }
+    }
+}
