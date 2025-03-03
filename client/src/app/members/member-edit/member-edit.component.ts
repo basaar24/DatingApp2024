c6ce2ae23a410f5ff29d9c3df045dd9649f60264
eriki26 <erick.gabriel0204@gmail.com>
2025-03-03 00:04:46 -0600

    Parcial04 - Add SPA Photo Editor component

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,53 @@
+import { Component, HostListener, inject, OnInit, ViewChild } from '@angular/core';
+import { Member } from '../../_models/member';
+import { AccountService } from '../../_services/account.service';
+import { MembersService } from '../../_services/members.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { PhotoEditorComponent } from "../photo-editor/photo-editor.component";
+
+@Component({
+  selector: 'app-member-edit',
+  standalone: true,
+  imports: [TabsModule, FormsModule, PhotoEditorComponent],
+  templateUrl: './member-edit.component.html',
+  styleUrl: './member-edit.component.css'
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild("editForm") editForm?: NgForm;
+  @HostListener("window:beforeunload", ["event"]) notify($event: any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
+  member?: Member;
+  public accountService = inject(AccountService);
+  private membersService = inject(MembersService);
+  private toastr = inject(ToastrService);
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const user = this.accountService.currentUser();
+    if (!user) return;
+    this.membersService.getMember(user.username).subscribe({
+      next: member => this.member = member
+    })
+  }
+
+  updateMember() {
+    this.membersService.updateMember(this.editForm?.value).subscribe({
+      next: _ => {
+        this.toastr.success("Profile updated!");
+        this.editForm?.reset(this.member);
+      }
+    });
+  }
+
+  onMemberChange(event: Member) {
+    this.member = event;
+  }
+}
