e64608f6b6f998df4322767c043524f1ff87e5e0
Arturo Barajas <basaar.24@gmail.com>
2025-03-12 07:37:00 -0600

    Parcial05 - Add like toogle feature

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -15,4 +15,16 @@ export class MemberCardComponent {
   private likesService = inject(LikesService);
   member = input.required<Member>();
   hasLiked = computed(() => this.likesService.likeIds().includes(this.member().id));
+
+  toogleLike() {
+    this.likesService.toogleLike(this.member().id).subscribe({
+      next: () => {
+        if (this.hasLiked()) {
+          this.likesService.likeIds.update(ids => ids.filter(i => i !== this.member().id));
+        } else {
+          this.likesService.likeIds.update(ids => [...ids, this.member().id]);
+        }
+      }
+    });
+  }
 }
