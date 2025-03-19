000d0fab24cb4d60bbf8c19b5afd80bca77165d4
Arturo Barajas <basaar.24@gmail.com>
2025-03-19 07:46:08 -0600

    Parcial05 - Add AutoMapperProfile for message response

--- API/DTOs/MessageResponse.cs
+++ API/DTOs/MessageResponse.cs
@@ -8,7 +8,7 @@ public class MessageResponse
     public required string SenderPhotoUrl { get; set; }
     public int RecipientId { get; set; }
     public required string RecipientUsername { get; set; }
-    public required string RecipienPhotoUrl { get; set; }
+    public required string RecipientPhotoUrl { get; set; }
     public required string Content { get; set; }
     public DateTime? DateRead { get; set; }
     public DateTime MessageSent { get; set; }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -19,5 +19,10 @@ public class AutoMapperProfiles : Profile
         CreateMap<MemberUpdateRequest, AppUser>();
         CreateMap<RegisterRequest, AppUser>();
         CreateMap<string, DateOnly>().ConvertUsing(s => DateOnly.Parse(s, CultureInfo.InvariantCulture));
+        CreateMap<Message, MessageResponse>()
+            .ForMember(d => d.SenderPhotoUrl,
+                o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(p => p.IsMain)!.Url))
+            .ForMember(d => d.RecipientPhotoUrl,
+                o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(p => p.IsMain)!.Url));
     }
 }
